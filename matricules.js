e5e16da95a32243fcaba134ccd8c63eddf7369c5
lucasignor67-commits <lucasignor67@gmail.com>
2026-07-19 16:16:50 +0200

    Add files via upload

---
+++ matricules.js
@@ -0,0 +1,104 @@
+// ============================================================
+//  FICHIER DÉDIÉ : LISTE DES MATRICULES
+//  Les deux pages (index.html et voir-matricules-en-service.html)
+//  récupèrent la liste en appelant getMatricules().
+//  Pour modifier ici : change uniquement les lignes de matricules
+//  (01, 02, 03…) entre les deux repères, puis rouvre les pages.
+// ============================================================
+const LISTE_MATRICULES = `
+01  | Eren Ernesto Bueno Volpe 🪖🚁⚓->  General  :General: :ResponsableMarine: :ResponsableTerrestre: :ResponsableFuerza:
+02 |
+03 |
+04 | Frost Alex 🚁                                   ->  Teniente en test :Teniente: :AspirantFuerza:
+05 | Teixeira Maxime ⚓                        ->  Soldado  :Soldado:
+06 | Emax Jackson 🪖                           ->  Capitan Primero  :CapitanPrimero:
+07 | Marc Bueno ⚓                                -> Sargento Primero  :CapitanPrimero:
+08 | Rio Ochoa ▫️                                   -> Recluta :Recluta:
+09 | Nueve Ramirez ⚓                          -> Comandante :Commandante~1: :AspirantMarine:
+10 | Nino Ramirez ⚓                              -> Capitan Segundo :CapitanSegundo:
+11 | Max Emerson 🚁                               -> Capitan Segundo :CapitanSegundo:
+12 | Zarouk Kekya ▫️                              -> Recluta :Recluta:
+13 | Solís Grenadine ⚓                          -> Alfarez Segundo en Test :AlfarezSegundo:
+14 | Carlo Avarro 🚁                                -> Teniente :Teniente:
+15 | Looping Lee 🪖                                 -> Teniente Coronel :TenienteCoronel::AdjointTerrestre:
+16 | Esmée Bueno ⚓                              -> Coronel :Coronel~1: :ResponsableMarine:
+17 |  Gul Carlos 🪖                                    -> Sargento Primero :SargentoPrimero:
+18 | Nolan wood ▫️                                   -> Recluta :Recluta:
+19 | Brian Peterson 🚁                           -> Recluta :Recluta:
+20 | Martinez Kira 🚁                        -> Sergento en Test :Sargento: :AspirantFuerza:
+21 |
+22 | Alvaro cortes 🚁                          -> Recluta :Recluta:
+23 | Roméro Hiedler 🪖                     ->  Coronel en Test :Coronel~1: :ResponsableTerrestre:
+24 | Gouru Rayan ▫️                          -> Recluta :Recluta:
+25 | Jan Nowak ▫️                              -> Recluta :Recluta:
+26 | Malik Abik ▫️                               -> Recluta :Recluta:
+27 |
+28 | Vodjio Torres ⚓                           -> Alfarez Segundo en Test :AlfarezSegundo: :AspirantMarine:
+29 | Aylan thoands ▫️                        -> Recluta :Recluta:
+30 | Damon Peterson ▫️                   -> Recluta :Recluta:
+31  | Wolf Smith Nolan 🚁                      -> Cabo :Cabo:
+32 |
+33 | Hamza Bouzid ⚓                           -> Recluta :Recluta:
+34 | Zayne Alvarez ⚓                            -> Teniente Coronel :TenienteCoronel: :AdjointMarine:
+35 | Zed Alvarez 🚁                                -> Teniente Coronel :TenienteCoronel: :AdjointFuerza:
+36 | Diego Alvarez ▫️                            -> Recluta :Recluta:
+37 | jhonn Sléman ▫️                             -> Recluta :Recluta:
+38 | Alvaro Owen ⚓                              -> Soldado :Soldado:
+39 | Meylan Chihab 🪖                         -> Alfarez Segundo en Test :AlfarezSegundo: :AspirantTerrestre:
+40 |
+41  |
+42 | Hamilton Andy ⚓                         -> Soldado :Soldado:
+43 | Laponne Mattéo 🚁                      -> Coronel :Coronel~1: :ResponsableFuerza:
+44 | Sergio Artys 🚁                              -> Teniente :Teniente:
+45  |
+46 | Tom KIRKMANT ▫️                      -> Recluta :Recluta:
+47 | Nass Bouzid  ▫️                              -> Recluta :Recluta:
+48 | Callisto Reyes 🪖                           -> Sergento Primero :AlfarezPrimero:
+49 |
+50 | Tony Garcia Galvino 🪖                -> Teniente en Test :Teniente:
+51  | Diego Ramirez ▫️                          -> Recluta :Recluta:
+52 |
+53 | Frost Thommas ⚓                       -> Soldado :Soldado:
+54 |
+55 | Pablito Escanor ▫️                       -> Recluta :Recluta:
+56 |
+57 | Thiago Rashford Muani ⚓         -> Soldado :Soldado:
+58 | Souleymane Mané ▫️                  -> Recluta :Recluta:
+59 | Gianni Lampuza 🚁                       -> Sargento :Sargento:
+60 |
+61 | Ibrahim Tohands ▫️                         -> Recluta :Recluta:
+62 | Valesco Riley 🪖                              -> Alfarez Primero :AlfarezPrimero: :AspirantTerrestre:
+63 |
+64 | Lopesse karl  ▫️                              -> Recluta :Recluta:
+65 | Wolf Mike ⚓                                    -> Alfarez Primero :AlfarezPrimero:
+66 | Hakim Mahgoumgo ⚓                 -> Soldado :Soldado:
+67 | james bobby ▫️                              -> Recluta :Recluta:
+68 | Rivera Emilio 🪖                             -> Alfarez Primero en Test :AlfarezPrimero:
+69 | Clément Landy ▫️                -> Recluta :Recluta:
+70 | Paqueta Fernandes Alvarez 🚁 -> Soldado :Soldado:
+71  | Mathis Luke ▫️                                 -> Recluta :Recluta:
+72 | Clark WHITER 🚁                            -> Soldado :Soldado:
+73 |
+74 | Lucky Luke 🪖                                   -> Soldado :Soldado:
+75 |
+76 I
+77 | livio santos 🪖                                   -> Soldado Primera :SoldadoPrimera:
+78 |
+79 |
+80 | Eren Kohlman ▫️                            -> Recluta :Recluta:
+81  |
+82 | Bernardo Wedson 🚁                     -> Soldado Primera :SoldadoPrimera:
+83 |
+84 |
+85 | Jhony Lamar ▫️                              -> Recluta :Recluta:
+86 |
+87 |
+88 | Fox Nina ⚓                                      ﻿ -> Sergento Primero :SargentoPrimero:
+89 | Jason Larkay 🪖                              -> Recluta :Recluta:
+90 | Freya Myers ▫️                                 -> Recluta :Recluta:
+`; // ⚠ NE PAS TOUCHER CETTE LIGNE (elle ferme la liste)
+
+// Fonction appelée par les deux pages pour récupérer la liste.
+function getMatricules() {
+  return LISTE_MATRICULES;
+}
